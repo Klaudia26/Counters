ff3544012abd5f6045f33b975ccad012d8bf0318
Klaudia <klaudia.krzywda@gmail.com>
2020-04-24 10:13:46 +0100

    add Counters section

--- src/components/Counters/Counters.js
+++ src/components/Counters/Counters.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Card from '../Card/Card';
+import './Counters.scss';
 
-const Counters = () => {
+const Counters = ({ cards, handleClick }) => {
   return (
-    <div>
-      Counters <Card />
+    <div className="counters">
+      {cards.map((card, index) => {
+        return <Card card={card} index={index} handleClick={handleClick} />;
+      })}
     </div>
   );
 };
